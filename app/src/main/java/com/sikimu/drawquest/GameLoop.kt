d5bf05892be394c3a8bca7172e7c2d650e81cf4b
sikimu <sikimu.pg@gmail.com>
2023-03-26 18:02:37 +0900

    ループコントローラーは不要と判断して削除

--- app/src/main/java/com/sikimu/drawquest/GameLoop.kt
+++ app/src/main/java/com/sikimu/drawquest/GameLoop.kt
@@ -1,11 +1,14 @@
 package com.sikimu.drawquest
 
 import android.view.MotionEvent
+import com.sikimu.drawquest.game.Game
+import com.sikimu.drawquest.game.GameTitle
 
 class GameLoop(private val system: GameSystem) : Runnable {
     val motionEvent = GameMotionEvent(Vector2D(0F,0F) , GameMotionEvent.Action.FREE)
     private var isRunning = false
     private var lastUpdateTime = System.currentTimeMillis()
+    private var game: Game = GameTitle()
 
     override fun run() {
         if (isRunning) {
@@ -15,7 +18,8 @@ class GameLoop(private val system: GameSystem) : Runnable {
 
             // ゲームの状態を更新する処理を実装する
             // 経過時間をelapsedTimeで考慮して、1回分の更新を行う
-            val rectStorage = GameLoopController.update(system, motionEvent)
+            game = game.update(system, motionEvent)
+            val rectStorage = game.createStorage()
             system.draw(rectStorage)
 
             //Downを解除する
