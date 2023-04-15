9b38f4c39751efdeb20b700ae95fa281d2b3db1a
sikimu <sikimu.pg@gmail.com>
2023-04-15 22:19:28 +0900

    選択用のウィンドウを描画クラスに移動

--- app/src/main/java/com/sikimu/drawquest/drawer/BattleDrawer.kt
+++ app/src/main/java/com/sikimu/drawquest/drawer/BattleDrawer.kt
@@ -45,11 +45,29 @@ class BattleDrawer {
             Color.GREEN
         )
 
+        private val selectWindow = StrokeRectData(
+            DrawAreaData(
+                -500F,
+                400F,
+                1000F,
+                400F
+            ) ,
+            Color.WHITE,
+            10F
+        )
+
         /**
          * 敵のウィンドウをまとめて描画する描画データ配列の作成
          */
         fun createEnemyWindow(): List<DrawData> {
             return listOf(enemySky , enemyField, enemyWindow)
+        }
+
+        /**
+         * 選択ウィンドウを描画する描画データの作成
+         */
+        fun createSelectWindow(): DrawData {
+            return selectWindow
         }
     }
 }
--- app/src/main/java/com/sikimu/drawquest/game/GameBattle.kt
+++ app/src/main/java/com/sikimu/drawquest/game/GameBattle.kt
@@ -8,19 +8,6 @@ import com.sikimu.drawquest.drawer.BattleDrawer
 import com.sikimu.drawquest.drawer.RectEnemyDrawer
 
 class GameBattle : Game() {
-
-    //選択ウィンドウ
-    private val selectWindow = StrokeRectData(
-        DrawAreaData(
-            -500F,
-            400F,
-            1000F,
-            400F
-        ) ,
-        Color.WHITE,
-        10F
-    )
-
     override fun update(motionEvent: GameMotionEvent): Game {
         return this
     }
@@ -29,7 +16,7 @@ class GameBattle : Game() {
         // ゲーム画面の四角形を含めた全ての四角形を返す
         val storage = DrawingDataStorage(Color.BLACK)
         storage.addRectAll(BattleDrawer.createEnemyWindow())
-        storage.addRect(selectWindow)
+        storage.addRect(BattleDrawer.createSelectWindow())
         storage.addRect(RectEnemyDrawer.create(DrawParam.Pixel(0F), DrawParam.Pixel(-300F)))
         return storage
     }
