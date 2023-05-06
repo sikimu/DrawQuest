a6b7bd089b2b1994040827cf03941a0360d15cc5
sikimu <sikimu.pg@gmail.com>
2023-05-06 20:06:51 +0900

    コメント追加

--- app/src/main/java/com/sikimu/drawquest/drawdata/FillStrokeCenterRectData.kt
+++ app/src/main/java/com/sikimu/drawquest/drawdata/FillStrokeCenterRectData.kt
@@ -7,11 +7,14 @@ import android.graphics.RectF
 data class FillStrokeCenterRectData(val drawPoint : DrawPoint , val drawSize : DrawSize , val fillColor: Int, val strokeColor: Int) : DrawData() {
 
     override fun draw(canvas : Canvas , paint : Paint) {
+        //塗りつぶし
         paint.color = fillColor
         paint.style = Paint.Style.FILL
         val area = DrawAreaCalculator.calcAreaCenter(drawPoint, drawSize, canvas.width, canvas.height)
         val rect = RectF(area.left, area.top, area.right, area.bottom)
         canvas.drawRect(rect, paint) // rectListに格納された矩形を描画
+
+        //枠
         paint.color = strokeColor
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = 10F
