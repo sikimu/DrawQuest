e354120daa7176d7cd028fa4892103fd360f2430
sikimu <sikimu.pg@gmail.com>
2023-05-07 01:05:44 +0900

    当たり判定のテストを実装

--- app/src/main/java/com/sikimu/drawquest/game/GameMain.kt
+++ app/src/main/java/com/sikimu/drawquest/game/GameMain.kt
@@ -25,22 +25,43 @@ class GameMain(private val worldData : WorldData) : Game() {
      */
     override fun update(motionEvent: GameMotionEvent): Game {
 
+        //次の状態遷移を記録しておく
+        var nextGame : Game = this
+
         worldData.player.update(motionEvent)
 
-        worldData.playerCenter += worldData.player.getDelta()
-        worldData.cameraCenter = worldData.playerCenter
+        movePlayer()
 
         // プレイヤーと敵が近すぎる場合、プレイヤーの位置を調整する
-        if ((worldData.playerCenter - worldData.enemyCenter).magnitude() < (RectPlayer.WIDTH / 2 + RectEnemyDrawer.WIDTH / 2)) {
-            worldData.playerCenter += ((worldData.playerCenter - worldData.enemyCenter).normalize() * (RectPlayer.WIDTH / 2 + RectEnemyDrawer.WIDTH / 2) - (worldData.playerCenter - worldData.enemyCenter))
-            return GameBattleIn(worldData)
+        if ((worldData.playerCenter - worldData.enemyCenter).magnitude() < RectEnemyDrawer.WIDTH) {
+            hitBack()
+            nextGame = GameBattleIn(worldData)
         }
 
         // 敵の表示位置を更新する
         enemyViewCenterX = DrawParam.Pixel(worldData.enemyCenter.x - worldData.cameraCenter.x)
         enemyViewCenterY = DrawParam.Pixel(worldData.enemyCenter.y - worldData.cameraCenter.y)
 
-        return this
+        return nextGame
+    }
+
+    /**
+     * 当たった分戻す
+     */
+    fun hitBack() {
+        val playerToEnemy = worldData.playerCenter - worldData.enemyCenter
+        val desiredDistance = (RectPlayer.WIDTH / 2 + RectEnemyDrawer.WIDTH / 2)
+        val correctionVector = playerToEnemy.normalize() * desiredDistance
+        val displacement = correctionVector - playerToEnemy
+        worldData.playerCenter += displacement
+    }
+
+    /**
+     * プレイヤーの移動
+     */
+    private fun movePlayer() {
+        worldData.playerCenter += worldData.player.getDelta()
+        worldData.cameraCenter = worldData.playerCenter
     }
 
     /**
--- app/src/test/java/com/sikimu/drawquest/game/GameMainTest.kt
+++ app/src/test/java/com/sikimu/drawquest/game/GameMainTest.kt
@@ -1,9 +1,13 @@
 package com.sikimu.drawquest.game
 
+import com.sikimu.drawquest.RectPlayer
+import com.sikimu.drawquest.Vector2D
 import com.sikimu.drawquest.WorldData
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
+import com.sikimu.drawquest.drawer.RectEnemyDrawer
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import kotlin.math.abs
 
 class GameMainTest {
 
@@ -17,5 +21,94 @@ class GameMainTest {
         val result = gameMain.createStorage()
         assertNotNull(result.getStorage())
         assertEquals(2, result.getStorage().size)
+    }
+
+    @Test
+    @DisplayName("左から接触")
+    fun testHitBackLeft() {
+        val worldData = WorldData()
+        val gameMain = GameMain(worldData)
+
+        worldData.playerCenter = Vector2D(50F, 250F)
+        worldData.enemyCenter = Vector2D(100F, 250F)
+        gameMain.hitBack()
+
+        //縦横のどちらがか当たり判定からズレていればOK
+        val result = worldData.playerCenter - worldData.enemyCenter
+        //TODO エネミーの幅が不適切(現状移動先がない)
+        //TODO 縦幅もない
+        val resultX = abs(result.x) >= RectPlayer.WIDTH / 2 + RectEnemyDrawer.WIDTH / 2
+        val resultY = abs(result.y) >= RectPlayer.HEIGHT / 2 + RectEnemyDrawer.WIDTH / 2
+        assertTrue(resultX || resultY)
+    }
+
+    @Test
+    @DisplayName("右から接触")
+    fun testHitBackRight() {
+        val worldData = WorldData()
+        val gameMain = GameMain(worldData)
+
+        worldData.playerCenter = Vector2D(50F, 250F)
+        worldData.enemyCenter = Vector2D(100F, 250F)
+        gameMain.hitBack()
+
+        //縦横のどちらがか当たり判定からズレていればOK
+        val result = worldData.playerCenter - worldData.enemyCenter
+        //TODO エネミーの幅が不適切(現状移動先がない)
+        //TODO 縦幅もない
+        val resultX = abs(result.x) >= RectPlayer.WIDTH / 2 + RectEnemyDrawer.WIDTH / 2
+        val resultY = abs(result.y) >= RectPlayer.HEIGHT / 2 + RectEnemyDrawer.WIDTH / 2
+        assertTrue(resultX || resultY)
+    }
+
+    @Test
+    @DisplayName("下から接触")
+    fun testHitBackTop() {
+        val worldData = WorldData()
+        val gameMain = GameMain(worldData)
+
+        worldData.playerCenter = Vector2D(50F, 250F)
+        worldData.enemyCenter = Vector2D(50F, 200F)
+        gameMain.hitBack()
+
+        //縦横のどちらがか当たり判定からズレていればOK
+        val result = worldData.playerCenter - worldData.enemyCenter
+        //TODO エネミーの幅が不適切(現状移動先がない)
+        //TODO 縦幅もない
+        val resultX = abs(result.x) >= RectPlayer.WIDTH / 2 + RectEnemyDrawer.WIDTH / 2
+        val resultY = abs(result.y) >= RectPlayer.HEIGHT / 2 + RectEnemyDrawer.WIDTH / 2
+        assertTrue(resultX || resultY)
+    }
+
+    @Test
+    @DisplayName("上から接触")
+    fun testHitBackBottom() {
+        val worldData = WorldData()
+        val gameMain = GameMain(worldData)
+
+        worldData.playerCenter = Vector2D(50F, 150F)
+        worldData.enemyCenter = Vector2D(50F, 200F)
+        gameMain.hitBack()
+
+        //縦横のどちらがか当たり判定からズレていればOK
+        val result = worldData.playerCenter - worldData.enemyCenter
+        //TODO エネミーの幅が不適切(現状移動先がない)
+        //TODO 縦幅もない
+        val resultX = abs(result.x) >= RectPlayer.WIDTH / 2 + RectEnemyDrawer.WIDTH / 2
+        val resultY = abs(result.y) >= RectPlayer.HEIGHT / 2 + RectEnemyDrawer.WIDTH / 2
+        assertTrue(resultX || resultY)
+    }
+
+    @Test
+    @DisplayName("右上から接触(今は中心からの距離でしか見ていないため重なりは残る)")
+    fun testHitBackRightTop() {
+        val worldData = WorldData()
+        val gameMain = GameMain(worldData)
+
+        worldData.playerCenter = Vector2D(50F, 150F)
+        worldData.enemyCenter = Vector2D(100F, 200F)
+        gameMain.hitBack()
+
+        assertTrue((worldData.playerCenter - worldData.enemyCenter).magnitude() >= RectEnemyDrawer.WIDTH)
     }
 }
